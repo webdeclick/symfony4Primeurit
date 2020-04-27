13613ef37f4f76bc1be4a16157f8423eee26c2cc
de-moor.vincent <vincent.demoor68@gmail.com>
2020-04-27 09:40:23 +0200

    php bin/console make:entity Categories MotsCles php bin/console make:migration php bin/console doctrine:migrations:migrate

---
+++ src/Entity/MotsCles.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MotsClesRepository")
+ */
+class MotsCles
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $mot_cle;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $slug;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMotCle(): ?string
+    {
+        return $this->mot_cle;
+    }
+
+    public function setMotCle(string $mot_cle): self
+    {
+        $this->mot_cle = $mot_cle;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
