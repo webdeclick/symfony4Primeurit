bbe27d23699a8550b6c26b8d2c592ff2e2191a00
de-moor.vincent <vincent.demoor68@gmail.com>
2020-04-29 15:58:56 +0200

    modification entité articles gedmo timestampable

--- src/Entity/Articles.php
+++ src/Entity/Articles.php
@@ -36,11 +36,17 @@ class Articles
     private $contenu;
 
     /**
+     * @var \DateTime $created_at
+     * 
+     * @Gedmo\Timestampable(on="create")
      * @ORM\Column(type="datetime")
      */
     private $created_at;
 
     /**
+     * @var \DateTime $updated_at
+     * 
+     * @Gedmo\Timestampable(on="update")
      * @ORM\Column(type="datetime")
      */
     private $updated_at;
@@ -116,24 +122,10 @@ class Articles
     {
         return $this->created_at;
     }
-
-    public function setCreatedAt(\DateTimeInterface $created_at): self
-    {
-        $this->created_at = $created_at;
-
-        return $this;
-    }
-
+    
     public function getUpdatedAt(): ?\DateTimeInterface
     {
         return $this->updated_at;
-    }
-
-    public function setUpdatedAt(\DateTimeInterface $updated_at): self
-    {
-        $this->updated_at = $updated_at;
-
-        return $this;
     }
 
     public function getFeaturedImage(): ?string
