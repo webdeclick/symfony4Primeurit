7071060fa9b504c216b804344845fea561a6fe92
de-moor.vincent <vincent.demoor68@gmail.com>
2020-04-29 22:47:53 +0200

    rectif upload

--- src/Entity/Articles.php
+++ src/Entity/Articles.php
@@ -6,7 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
-use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\File as File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
