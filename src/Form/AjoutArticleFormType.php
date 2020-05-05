61d78dc2db304e9d072cff807d96222ffd1e450a
de-moor.vincent <vincent.demoor68@gmail.com>
2020-05-05 23:01:39 +0200

    php bin/console make:form ajoutArticleFormType

---
+++ src/Form/AjoutArticleFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Articles;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AjoutArticleFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('slug')
+            ->add('contenu')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('featured_image')
+            ->add('users')
+            ->add('mots_cles')
+            ->add('categories')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Articles::class,
+        ]);
+    }
+}
