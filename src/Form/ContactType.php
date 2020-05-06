aab735bb72533758c41b0c261ee3ca7b35190a75
de-moor.vincent <vincent.demoor68@gmail.com>
2020-05-07 00:09:50 +0200

    ajout des champs du formulaire de contact

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,7 +14,10 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('field_name')
+            ->add('nom')
+            ->add('email', EmailType::class)
+            ->add('message', TextareaType::class)
+            ->add('envoyer', SubmitType::class)
         ;
     }
 
