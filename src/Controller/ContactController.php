8e45b903776400fceef9a70339e899906c2c9ccc
de-moor.vincent <vincent.demoor68@gmail.com>
2020-05-13 23:45:01 +0200

    envoi email

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use Swift_Mailer;
+use Swift_Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -10,10 +14,31 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index()
+    public function index(Request $request, \Swift_Mailer $mailer)
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $contact = $form->getData();
+
+            $message = (new \Swift_Message('Nouveau contact'))
+            ->setFrom($contact['email'])
+            ->setTo('votre@adresse.fr')
+            ->setBody(
+                $this->renderView(
+                    'emails/contact.html.twig', compact('contact')
+                ),
+                'text/html'
+            );
+
+            $mailer->send($message);
+
+            $this->addFlash('message','Le message à bien été envoyé');
+            return $this->redirectToRoute('contact');
+        }
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'contactForm' => $form->createView(),
         ]);
     }
 }
